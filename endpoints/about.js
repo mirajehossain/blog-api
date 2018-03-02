d1a809fe0573b3b2d6f0a318df5e9c285d7fed50
Miraje <mdalamin8521@gmail.com>
2018-03-03 02:03:39 +0600

    All crud done for about!!

--- endpoints/about.js
+++ endpoints/about.js
@@ -1,21 +1,68 @@
-const AboutModel = require('../models/post');
+const AboutModel = require('../models/about');
 
 let about= {};
 
 about.GetAbout =  (req, res) => {
-    // PostModel.getPost()
+    AboutModel.getAbout(function (error, result) {
+        if(error)
+        // throw new Error(error) ;
+            throw error;
+
+        res.json({
+            success:true,
+            message:"Getting data Successfully.",
+            data:result
+        });
+    })
 };
 
 about.AddAbout = (req, res) => {
 
+    let about = req.body;
+    AboutModel.addAbout(about,function (error, result) {
+        if(error)
+        // throw new Error(error) ;
+            throw error;
+
+        res.json({
+            success:true,
+            message:"Getting data Successfully.",
+            data:result
+        });
+    })
 };
 
 about.UpdateAbout = (req, res) => {
 
+    let id= req.params.id;
+    let data = req.body;
+    AboutModel.updateAbout(id,data,function (error, result) {
+        if(error)
+        // throw new Error(error) ;
+            throw error;
+
+        res.json({
+            success:true,
+            message:"About Updated Successfully.",
+            data:result
+        });
+    })
 };
 
 about.DeleteAbout = (req, res) => {
 
+    let id= req.params.id;
+    AboutModel.deleteAbout(id,function (error, result) {
+        if(error)
+        // throw new Error(error) ;
+            throw error;
+
+        res.json({
+            success:true,
+            message:"Delete about Successfully.",
+            data:result
+        });
+    })
 };
 
 module.exports = about;
