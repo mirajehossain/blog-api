623e03f3ee35b8f382c134c9215ad1605c4db15a
Miraje <mdalamin8521@gmail.com>
2018-03-03 01:35:06 +0600

    All crud done for Category!!

--- endpoints/category.js
+++ endpoints/category.js
@@ -1,22 +1,67 @@
-const PostModel = require('../models/post');
+const CategoryModel = require('../models/category');
 
 let category = {};
 
 category.GetCategoies = (req, res) => {
-    // PostModel.getPost()
+    CategoryModel.getCategory(function (error, result) {
+        if(error)
+        // throw new Error(error) ;
+            throw error;
+
+        res.json({
+            success:true,
+            message:"Getting All Category Successfully.",
+            data:result
+        });
+
+    })
 };
 
 category.AddCategory = (req, res) => {
+    let category = req.body;
+    CategoryModel.addCategory(category,function (error, result) {
+        if(error)
+        // throw new Error(error) ;
+            throw error;
 
+        res.json({
+            success:true,
+            message:"New Category added Successfully.",
+            data:result
+        });
+    })
 };
 
 category.UpdateCategory = (req, res) => {
+    let id = req.params.id;
+    let category = req.body;
+    CategoryModel.updateCategory(id,category,function (error, result) {
+        if(error)
+        // throw new Error(error) ;
+            throw error;
 
+        res.json({
+            success:true,
+            message:"Updated Category Successfully.",
+            data:result
+        });
+    })
 };
 
 
 category.DeleteCategory = (req, res) => {
+    let id = req.params.id;
+    CategoryModel.deleteCategory(id,function (error, result) {
+        if(error)
+        // throw new Error(error) ;
+            throw error;
 
+        res.json({
+            success:true,
+            message:"Deleted Category Successfully.",
+            data:result
+        });
+    })
 };
 
 module.exports = category;
